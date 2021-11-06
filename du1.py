a13eb9966ea84cd0a7df6c8d7d30518437065533
pmojmir <93137745+pmojmir@users.noreply.github.com>
2021-11-07 00:26:43 +0100

    Update du1.py

--- du1.py
+++ du1.py
@@ -4,72 +4,78 @@ from turtle import circle, forward, left, right, penup, pendown, setpos, exitonc
 print("Dobrý den, vítejte u hry piškvorky! Hra je určena pro dva hráče, můžete si zvolit libovolnou velikost hracího pole")
 
 #Nastavení velikosti hracího pole
-nx = (int(input("Zadej velikost hracího pole strany x: ")))
-ny = (int(input("Zadej velikost hracího pole strany y: ")))
+velikost_strany = (int(input("Nastav velikost strany čtverce (hodnota větší než 20, doporučuji 100): ")))
+while velikost_strany < 20:
+    print("Moc malé pole")
+    velikost_strany = (int(input("Zadej znovu velikost čtverce 20 a víc ")))
 
+#Počet hracích polí 
+nx = (int(input("Zadej velikost hracího pole strany X: ")))
+ny = (int(input("Zadej velikost hracího pole strany Y: ")))
 #Ošetření vstupů 
 while nx <=0:
     print("Velikost strany hracího pole musí být kladné nenulové číslo ")
-    nx = (int(input("Zadej znovu velikost hracího pole strany x: ")))
+    nx = (int(input("Zadej znovu velikost hracího pole strany X: ")))
 while ny <=0:
     print("Velikost strany hracího pole musí být kladné nenulové číslo ")
-    ny = (int(input("Zadej znovu velikost hracího pole strany y: ")))
+    ny = (int(input("Zadej znovu velikost hracího pole strany Y: ")))
 
 #Nastavení rychlosti vykreslovní, pomocné proměnné velikost a velikosti čtverce
 speed = 10
 velikost = nx*ny
-a = 100
 
 #Kreslení hracího pole
 for _ in range(ny):
 	for _ in range(nx):
 		# Kreslení čtverce
 		for _ in range(4):
-			forward(a)
+			forward(velikost_strany)
 			left(90)
 		# Posun na další
-		forward(a)
+		forward(velikost_strany)
 	left(180)
-	forward(nx*a)
+	forward(nx*velikost_strany)
 	right(90)
-	forward(a)
+	forward(velikost_strany)
 	right(90)
 
 #Tělo hry 
 #For cyklus pro opakovaní dotazů na kreslení, počet kol ošetřen proměnou velikost 
 for i in range(velikost):
     #Podmínka na střídání hráčů
+    print("Kolo", i+1)
+    x = (int(input("Zadej souřadnici X v kartézském systému: ")))
+    y = (int(input("Zadej souřadnici Y v kartézském systému: ")))
+     #Ošetření vstupů 
+    while x > nx or x <= 0:
+        print("Hraješ mimo hrací pole")
+        x = (int(input("Zadej znovu souřadnici X v kartézském systému: ")))
+    while y > ny or y <= 0:
+        print("Hraješ mimo hrací pole")
+        y = (int(input("Zadej znovu souřadnici Y v kartézském systému: ")))
+    #Převedení souřadnic na počátek 
+    xx = x*velikost_strany-(velikost_strany/2)
+    yy = y*velikost_strany-(velikost_strany/2)
+    #Křížek nebo koločko
     if i % 2 == 0:
-        print("Kolo", i+1, "Hraje první hráč, hraje s kolečky, souřadnice jsou kartézském systému, např. levý dolní roh má 1,1")
-        x = (int(input("Zadej souřadnici X v kartézském systému: ")))
-        y = (int(input("Zadej souřadnici Y v kartézském systému: ")))
-        #Ošetření vstupů 
-        while x > nx or x <= 0:
-            print("Hraješ mimo hrací pole")
-            x = (int(input("Zadej znovu souřadnici X v kartézském systému: ")))
-        while y > ny or y <= 0:
-            print("Hraješ mimo hrací pole")
-            y = (int(input("Zadej znovu souřadnici  Y v kartézském systému: ")))
-        #Převedení souřadnic na počátek
-        xx = x*a-(a/2)
-        yy = y*a-(a/2)
         #Kreslení kolečka 
-        kresleni = (penup(),setpos(xx,yy),pendown(),circle(10), penup())   
+        penup()
+        setpos(xx,yy)
+        pendown()
+        circle(velikost_strany/5)
+        penup() 
     else: 
-        print("Kolo", i+1, "Hraje druhý hráč, hraje křížky, souřadnice jsou kartézském systému, např. levý dolní roh má 1,1")
-        x = (int(input("Zadej souřadnici Y v kartézském systému: ")))
-        y = (int(input("Zadej souřadnici Y v kartézském systému: ")))
-        while x > nx or x <= 0:
-            print("Hraješ mimo hrací pole")
-            x = (int(input("Zadej znovu souřadnici X v kartézském systému: ")))
-        while y > ny or y <= 0:
-            print("Hraješ mimo hrací pole")
-            y = (int(input("Zadej znovu souřadnici Y v kartézském systému: ")))
-        xx = x*a-(a/2)
-        yy = y*a-(a/2)
         #Kreslení křížku 
-        kresleni2 = (setpos(xx,yy), pendown()) 
-        krizek = (right(45),forward(20), backward(40), forward(20), left(90), forward(20), backward(40), right(45))
+        setpos(xx,yy)
+        pendown()
+        right(45)
+        forward(velikost_strany/5)
+        backward(velikost_strany/2.5)
+        forward(velikost_strany/5) 
+        left(90) 
+        forward(velikost_strany/5)
+        backward(velikost_strany/2.5)
+        right(45)
+       
 print("Konec hry")
-exitonclick()
-		
+exitonclick()		
